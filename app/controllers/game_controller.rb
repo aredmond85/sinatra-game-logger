42513fa1b04ebc6326c220788ee0517e919c48a2
Austin Redmond <aredmond85@gmail.com>
2020-06-13 11:56:57 -0500

    added authentication, authorization, and creation

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,20 +1,46 @@
 class GameController < ApplicationController
   get '/games' do
     authenticate
-    @games = current_user
-    erb :'/games/index'
+    if logged_in?
+      @games = current_user
+      erb :'/games/index'
+    else
+      redirect to '/login'
+    end
   end
 
   get '/games/new' do
     authenticate
-    erb :'/games/new'
+    if logged_in?
+      erb :'/games/new'
+    else
+      redirect to '/login'
+    end
   end
 
-  post '/games/new' do 
-    
+  post '/games' do
+    authenticate
+    if logged_in?
+      if params[:title] == "" || params[:console] == "" || params[:status] == ""
+        redirect to "/games/new"
+      else
+        @game = Game.create(title: params[:title], console: params[:console], status: params[:status])
+        @game.user = current_user
+        if @game.save
+          redirect to "/games"
+          #redirect to "/games/#{@game.id}"
+        else
+          redirect '/games/new' 
+        end
+      end
+    else
+      redirect to '/login'
+    end
   end
 
   get '/games/show' do
-    
+    authenticate
+    erb :'games/show'
   end 
+
 end
