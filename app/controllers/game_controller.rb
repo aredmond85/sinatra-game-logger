96a25cc14c8ca3f782004837eeb0c2a06c1701a0
Austin Redmond <aredmond85@gmail.com>
2020-06-12 22:55:10 -0500

    added initial get and posts

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,2 +1,20 @@
 class GameController < ApplicationController
+  get '/games' do
+    authenticate
+    @games = current_user
+    erb :'/games/index'
+  end
+
+  get '/games/new' do
+    authenticate
+    erb :'/games/new'
+  end
+
+  post '/games/new' do 
+    
+  end
+
+  get '/games/show' do
+    
+  end 
 end
