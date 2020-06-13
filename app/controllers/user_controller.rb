d207ac79e0a2e6c3a65ce80946e069c8714a5fad
Austin Redmond <aredmond85@gmail.com>
2020-06-12 22:54:38 -0500

    configured and working

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,36 +1,48 @@
 class UserController < ApplicationController
 
-    get '/signup' do 
-        erb :'users/signup'
-    end
-
-    get '/login' do 
-        erb :'users/login'
-    end
-
-    post '/login' do 
-        user = User.find_by(username: params[:username])
-        if user && user.authenticate(params[:password]) 
-            session[:user_id] = user.id
-            redirect '/games'
+    get '/signup' do
+        if !logged_in?
+          erb :'users/signup', locals: {message: "Please sign up before you sign in"}
         else
-            erb :'sessions/login'
+          redirect to '/tweets'
         end
-    end
+      end
 
-
-    post '/signup' do 
-        @user = User.new(name: params[:name], password: params[:password], username: params[:username])
-        if @user.save
+    post '/signup' do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect to '/signup'
+          else
+            @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+            @user.save
             session[:user_id] = @user.id
-            redirect "/games"
+            redirect to '/games'
+          end
+    end
+
+    get '/login' do
+        if !logged_in?
+          erb :'users/login'
         else
-            erb :'users/signup'
+          redirect to '/games'
         end
     end
 
-    delete '/logout' do 
-        session.clear
-        redirect '/login'
+    post '/login' do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect to "/games"
+        else
+          redirect to '/signup'
+        end
+      end
+
+    post '/logout' do
+        if logged_in?
+          session.destroy
+          redirect to '/login'
+        else
+          redirect to '/'
+        end
     end
 end
