258d8d82bb5a8dba6c0a2fe6aebdfe80d320e35d
Austin Redmond <aredmond85@gmail.com>
2020-06-14 13:12:42 -0500

    created edit, delete routes

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,56 +1,77 @@
 class GameController < ApplicationController
-  get '/games' do
-    authenticate
-    if logged_in?
-      @games = current_user
-      @games = Game.all
-      erb :'/games/index'
-    else
-      redirect to '/login'
-    end
+	get '/games' do
+		authenticate
+		if logged_in?
+			@games = current_user
+			@games = Game.all
+			erb :'/games/index'
+		else
+			redirect to '/login'
+		end
   end
-
-  get '/games/new' do
-    authenticate
-    if logged_in?
-      erb :'/games/new'
-    else
-      redirect to '/login'
-    end
+  
+		get '/games/new' do
+		authenticate
+		if logged_in?
+			erb :'/games/new'
+		else
+			redirect to '/login'
+		end
   end
-
-  post '/games' do
-    authenticate
-    if logged_in?
-      if params[:title] == "" || params[:console] == "" || params[:status] == ""
-        redirect to "/games/new"
-      else
-        @game = Game.create(title: params[:title], console: params[:console], status: params[:status])
-        @game.user = current_user
-        if @game.save
-          redirect to "/games"
-          #redirect to "/games/#{@game.id}"
-        else
-          redirect '/games/new' 
-        end
-      end
-    else
-      redirect to '/login'
-    end
+  
+		post '/games' do
+		authenticate
+		if logged_in?
+			if params[:title] == "" || params[:console] == "" || params[:status] == ""
+				redirect to "/games/new"
+			else
+				@game = Game.create(title: params[:title], console: params[:console], status: params[:status])
+				@game.user = current_user
+				if @game.save
+					redirect to "/games/#{@game.id}"
+				else
+					redirect '/games/new' 
+				end
+			end
+		else
+			redirect to '/login'
+		end
   end
-
-   get '/games/:id' do
-     if logged_in?
-       @game = Game.find_by_id(params[:id])
-       erb :'/games/show'
-     else
-       redirect to '/login'
-     end
-   end
-
-   get '/games/show' do
-     authenticate
-     erb :'games/show'
-   end 
-
-end
+  
+		get '/games/show' do
+		authenticate
+		erb :'games/show'
+  end 
+  
+		get '/games/:id' do #loads show.erb to show game details
+		@game = Game.find_by(id: params[:id])
+		erb :'games/show'
+  end
+  
+		get '/games/:id/edit' do
+		authenticate
+		@game = Game.find_by(id: params[:id])
+		erb :'games/edit'
+  end
+  
+		patch '/games/:id' do
+		authenticate
+		if params[:title] == "" || params[:console] == "" || params[:status] == ""
+			@error = "Looks like something is wrong, try to fill out all the fields."
+			erb :'/games/edit'
+    end
+		@game = Game.find_by(id: params[:id])
+		@game.title = params[:title]
+		@game.console = params[:console]
+		@game.status = params[:status]
+		@game.save
+		redirect "/games/#{@game.id}"
+  end
+  
+		delete '/games/:id' do
+		authenticate
+		@game = Game.find_by(id: params[:id])
+		@game.delete
+		redirect "/games"
+	end
+	end
